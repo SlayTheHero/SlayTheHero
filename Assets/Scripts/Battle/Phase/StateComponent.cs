370b101057898b7debcbc8d73b230f8e262e1fdc
kimgyeran <kgh08813@gmail.com>
2025-01-07 20:30:08 +0900

    Update StateComponent.cs

--- Assets/Scripts/Battle/Phase/StateComponent.cs
+++ Assets/Scripts/Battle/Phase/StateComponent.cs
@@ -27,7 +27,6 @@ public class StateComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int next;
         if (m_is_stop)
             return;
         if (m_curState == null)
